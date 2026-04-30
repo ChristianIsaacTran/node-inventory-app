c8c9c80d8e1de0a1a668d67ed7aa548eff87ef4b
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-04-29 20:11:19 -0500

    WORKING: tableRecords.js, finished item_type and rarity_type table tableRecords
    Need to fill in weapons table, consumables table, and utilities table

---
+++ db/tableRecords.js
@@ -0,0 +1,29 @@
+/*
+    this module is to contain all of the individual records 
+    for each table to be injected into the SQL query in tableSetup.js.
+*/
+
+// item_rarity records
+const itemRarityRecords = [
+    `common`,
+    `uncommon`,
+    `rare`,
+    `epic`,
+    `legendary`,
+    `mythic`
+]
+
+// item type records
+const itemTypeRecords = [
+    `assault rifle`,
+    `shotgun`,
+    `submachine gun`,
+    `pistol`,
+    `sniper rifle`,
+    `marksman rifle`,
+    `explosive weapon`,
+    `special weapon`,
+    `utility`,
+    `consumable`
+]
+
